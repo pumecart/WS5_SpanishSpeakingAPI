737af61efe3552daa4b71710ff5d7949a54499f7
Pumehana Cartagena <pum3hanacarta@byui.edu>
2024-02-17 13:37:04 -0500

    wk6 updates

--- swagger.js
+++ swagger.js
@@ -7,6 +7,8 @@ const doc = {
   },
   host: 'ws5-spanishspeakingapi.onrender.com',
   schemes: ['https']
+  // host: 'localhost:8080',
+  // schemes: ['http']
 };
 
 const outputFile = './swagger.json';
